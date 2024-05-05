083ce694a72da63f82894ea43ce93274821a5337
bibek-ku-ray <bi.ray1225@gmail.com>
2024-05-05 23:52:24 +0545

    some changes dont know what

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://vldjlqye:Bz0o2AC3HlYBpIPs6qFdVXj2Lg-dIDaM@satao.db.elephantsql.com/vldjlqye");
     await client.connect();
     return client;
 }
